1b034d3a10fc4547b177fe411856dedf6cec1db0
Warren Friedl <wfriedl@gmail.com>
2017-01-30 17:13:55 -0600

    update typo
    map dotfiles/bin

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-DOTFILES=~/dofiles
+DOTFILES=~/dotfiles
 
 ln -s $DOTFILES/.vim/vimrc ~/.vimrc
+ln -s $DOTFILES/.vim ~/.vim
 ln -s $DOTFILES/.zshrc ~/.zshrc
+
+ln -s $DOTFILES/bin ~/bin
 
 mkdir -p ~/tmp
 
